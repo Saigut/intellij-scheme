7893bb0c32dc4847a18e14414589a344aff435cc
Saigut <gosaigut@gmail.com>
2019-02-24 19:41:45 +0800

    push missing file

---
+++ src/schemely/psi/impl/SchemeSpecialLiteral.java
@@ -0,0 +1,12 @@
+package schemely.psi.impl;
+
+import com.intellij.lang.ASTNode;
+
+
+public class SchemeSpecialLiteral extends SchemePsiElementBase
+{
+  public SchemeSpecialLiteral(ASTNode node)
+  {
+    super(node, "SchemeSpecialLiteral");
+  }
+}
