c6a444c0bf63a24466e3da948542a67139c86a49
Saigut <gosaigut@gmail.com>
2022-05-31 19:49:10 +0800

    Fix Code Style

--- src/main/settings/codeStyle/SchemeCodeStyleSettingsProvider.java
+++ src/main/settings/codeStyle/SchemeCodeStyleSettingsProvider.java
@@ -4,12 +4,19 @@ import com.intellij.lang.Language;
 import com.intellij.psi.codeStyle.CodeStyleSettingsProvider;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
+import com.intellij.openapi.options.Configurable;
 import org.jetbrains.annotations.NotNull;
 import main.SchemeLanguage;
 
 
 public class SchemeCodeStyleSettingsProvider extends CodeStyleSettingsProvider
 {
+  @NotNull
+  public Configurable createSettingsPage(CodeStyleSettings settings, CodeStyleSettings originalSettings)
+  {
+    return new SchemeFormatConfigurable(settings, originalSettings);
+  }
+
   @Override
   public CustomCodeStyleSettings createCustomSettings(CodeStyleSettings settings)
   {
