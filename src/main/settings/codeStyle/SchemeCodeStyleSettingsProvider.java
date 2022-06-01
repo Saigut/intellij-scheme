4139357b08ecd8ac082978140e6761bf5b7853af
Saigut <gosaigut@gmail.com>
2022-06-01 18:42:13 +0800

    Deal with deprecated API usage; Remove unused code and files

--- src/main/settings/codeStyle/SchemeCodeStyleSettingsProvider.java
+++ src/main/settings/codeStyle/SchemeCodeStyleSettingsProvider.java
@@ -1,20 +1,21 @@
 package main.settings.codeStyle;
 
 import com.intellij.lang.Language;
+import com.intellij.psi.codeStyle.CodeStyleConfigurable;
 import com.intellij.psi.codeStyle.CodeStyleSettingsProvider;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
-import com.intellij.openapi.options.Configurable;
 import org.jetbrains.annotations.NotNull;
 import main.SchemeLanguage;
 
 
 public class SchemeCodeStyleSettingsProvider extends CodeStyleSettingsProvider
 {
+  @Override
   @NotNull
-  public Configurable createSettingsPage(CodeStyleSettings settings, CodeStyleSettings originalSettings)
+  public CodeStyleConfigurable createConfigurable(@NotNull CodeStyleSettings settings, @NotNull CodeStyleSettings modelSettings)
   {
-    return new SchemeFormatConfigurable(settings, originalSettings);
+    return new SchemeFormatConfigurable(settings, modelSettings);
   }
 
   @Override
--- src/main/settings/codeStyle/SchemeFormatConfigurable.java
+++ src/main/settings/codeStyle/SchemeFormatConfigurable.java
@@ -3,31 +3,17 @@ package main.settings.codeStyle;
 import com.intellij.application.options.CodeStyleAbstractConfigurable;
 import com.intellij.application.options.CodeStyleAbstractPanel;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
-import main.SchemeBundle;
-import main.SchemeIcons;
-
-import javax.swing.Icon;
 
 
 public class SchemeFormatConfigurable extends CodeStyleAbstractConfigurable
 {
   public SchemeFormatConfigurable(CodeStyleSettings settings, CodeStyleSettings cloneSettings)
   {
-    super(settings, cloneSettings, SchemeBundle.message("title.scheme.code.style.settings"));
+    super(settings, cloneSettings, "Scheme");
   }
 
   protected CodeStyleAbstractPanel createPanel(CodeStyleSettings settings)
   {
     return new SchemeCodeStylePanel(getCurrentSettings(), settings);
-  }
-
-  public Icon getIcon()
-  {
-    return SchemeIcons.SCHEME_ICON;
-  }
-
-  public String getHelpTopic()
-  {
-    return "reference.settingsdialog.IDE.globalcodestyle.spaces";
   }
 }
