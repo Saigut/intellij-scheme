593dee675787234b1a4ea23cb43115677a575053
Saigut <gosaigut@gmail.com>
2022-06-11 20:19:18 +0800

    Improve Fold

--- src/main/fold/SchemeFoldingBuilder.java
+++ src/main/fold/SchemeFoldingBuilder.java
@@ -4,7 +4,9 @@ import com.intellij.lang.ASTNode;
 import com.intellij.lang.folding.FoldingBuilder;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import main.lexer.SchemeTokens;
 import main.psi.impl.SchemeBodyOfForm;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,7 +18,13 @@ public class SchemeFoldingBuilder implements FoldingBuilder
 {
   public String getPlaceholderText(ASTNode node)
   {
-    return "...";
+    if (node.getElementType() == SchemeTokens.LINE_COMMENT) {
+      return ";...";
+    } else if (node.getElementType() == SchemeTokens.BLOCK_COMMENT) {
+      return "#|...|#";
+    } else {
+      return "...";
+    }
   }
 
   @NotNull
@@ -38,16 +46,34 @@ public class SchemeFoldingBuilder implements FoldingBuilder
       descriptors.add(new FoldingDescriptor(node, node.getTextRange()));
     }
     ASTNode child = node.getFirstChildNode();
-    while (child != null)
-    {
-      appendDescriptors(child, descriptors);
-      child = child.getTreeNext();
+    while (child != null) {
+      if (child.getElementType() == SchemeTokens.LINE_COMMENT) {
+        ASTNode firstNode = child;
+        ASTNode lastNode = child;
+        while (child != null) {
+          if (child.getElementType() == SchemeTokens.LINE_COMMENT) {
+            lastNode = child;
+          } else if (child.getElementType() == SchemeTokens.WHITESPACE) {
+          } else {
+            break;
+          }
+          child = child.getTreeNext();
+        }
+        descriptors.add(new FoldingDescriptor(firstNode,
+                new TextRange(firstNode.getStartOffset(),
+                        lastNode.getTextRange().getEndOffset())));
+      } else if (child.getElementType() == SchemeTokens.WHITESPACE) {
+        child = child.getTreeNext();
+      } else {
+        appendDescriptors(child, descriptors);
+        child = child.getTreeNext();
+      }
     }
   }
 
   private boolean isFoldableNode(ASTNode node)
   {
     PsiElement element = node.getPsi();
-    return element instanceof SchemeBodyOfForm;
+    return (element instanceof SchemeBodyOfForm) || (node.getElementType() == SchemeTokens.BLOCK_COMMENT);
   }
 }
